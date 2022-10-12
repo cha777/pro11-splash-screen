b92805ffcb4d7e426137edb788e6ffb6004a4807
cha777 <chathuranga_wm@yahoo.com>
2022-10-13 00:20:50 +0530

    Implementing splash screen visibility toggling

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-const chokidar = require('chokidar');
+const NW = require('nw.gui');
+const fs = require('fs');
+const path = require('path');
+
+const win = NW.Window.get();
 
 const messageElement = document.querySelector('h3.message');
 messageElement.innerHTML = process.argv[2] || 'Application is initializing';
 
-// Initialize watcher.
-const watcher = chokidar.watch('file, dir, glob, or array', {
-  ignored: /(^|[\/\\])\../, // ignore dotfiles
-  persistent: true,
-  cwd: process.cwd(),
+const splashTmpFileName = 'temp.dat';
+const splashTmpPath = path.resolve(process.cwd(), splashTmpFileName);
+let splashDismissTimeout;
+
+fs.watch(process.cwd(), { persistent: true }, (event, who) => {
+  if (event === 'rename' && who === splashTmpFileName) {
+    if (fs.existsSync(splashTmpFileName)) {
+      win.show();
+      win.focus();
+    } else {
+      if (splashDismissTimeout) {
+        clearInterval(splashDismissTimeout);
+      }
+
+      win.hide();
+    }
+  }
 });
 
-// Something to use when events are received.
-const log = console.log.bind(console);
-// Add event listeners.
-watcher
-  .on('add', (path) => log(`File ${path} has been added`))
-  .on('change', (path) => log(`File ${path} has been changed`))
-  .on('unlink', (path) => log(`File ${path} has been removed`));
+if (!fs.existsSync(splashTmpPath)) {
+  fs.writeFileSync(splashTmpFileName, '');
 
-console.log(process.cwd());
+  splashDismissTimeout = setTimeout(() => {
+    fs.unlinkSync(splashTmpPath);
+  }, 10000);
+} else {
+  console.warn('splash is already showing');
+}
