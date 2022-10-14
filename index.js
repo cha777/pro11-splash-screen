db550fe8f67cca5e0667976b5c2ee0e056d82316
cha777 <chathuranga_wm@yahoo.com>
2022-10-14 23:04:33 +0530

    Splash screen toggling optimization

--- index.js
+++ index.js
@@ -5,33 +5,71 @@ const path = require('path');
 const win = NW.Window.get();
 
 const messageElement = document.querySelector('h3.message');
-messageElement.innerHTML = process.argv[2] || 'Application is initializing';
 
 const splashTmpFileName = 'temp.dat';
 const splashTmpPath = path.resolve(process.cwd(), splashTmpFileName);
 let splashDismissTimeout;
 
+const showSplashScreen = () => {
+  messageElement.innerHTML = getSplashScreenMsg() || '';
+  win.show();
+  win.focus();
+
+  splashDismissTimeout = setTimeout(() => {
+    fs.unlinkSync(splashTmpPath);
+  }, 10000);
+};
+
+const hideSplashScreen = () => {
+  if (splashDismissTimeout) {
+    clearInterval(splashDismissTimeout);
+  }
+
+  win.hide();
+};
+
+const getSplashScreenMsg = () => {
+  try {
+    const msgCode = fs.readFileSync(splashTmpPath, 'utf8');
+    let msg;
+
+    switch (msgCode) {
+      case '1':
+        msg = 'Application is updating. Please Wait ...';
+        break;
+
+      case '2':
+        msg = 'Application is Restarting ...';
+        break;
+
+      case '3':
+        msg = 'Something went wrong. Please reinstall the app.';
+        break;
+
+      case 0:
+      default:
+        msg = 'Application is initializing ...';
+        break;
+    }
+
+    return msg;
+  } catch (err) {
+    console.error('Error while generating splash screen message', err);
+  }
+};
+
 fs.watch(process.cwd(), { persistent: true }, (event, who) => {
   if (event === 'rename' && who === splashTmpFileName) {
     if (fs.existsSync(splashTmpFileName)) {
-      win.show();
-      win.focus();
+      showSplashScreen();
     } else {
-      if (splashDismissTimeout) {
-        clearInterval(splashDismissTimeout);
-      }
-
-      win.hide();
+      hideSplashScreen();
     }
   }
 });
 
 if (!fs.existsSync(splashTmpPath)) {
   fs.writeFileSync(splashTmpFileName, '');
-
-  splashDismissTimeout = setTimeout(() => {
-    fs.unlinkSync(splashTmpPath);
-  }, 10000);
 } else {
   console.warn('splash is already showing');
 }
